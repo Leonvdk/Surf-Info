6740755fcefde9457b01c0598dfb78b4e5cd50ce
leonvdk <leonvandeklundert@gmail.com>
2023-05-04 22:07:42 +0100

    Added scss support & started working on home hero

---
+++ components/HomeHero.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function HomeHero() {
+  return (
+    <section>
+        <div>
+            <h1>Surf-Info.</h1>
+        </div>
+        <div>
+            <ul>
+                <li>Let's go surfing</li>
+                <li>Let's go surfing</li>
+                <li>Let's go surfing</li>
+                <li>Let's go surfing</li>
+                <li>Let's go surfing</li>
+            </ul>
+            <p>Some motivting text</p>
+        </div>
+        <div>
+            <p>The text in the white sidebar</p>
+        </div>
+    </section>  
+  )
+}
+
+export default HomeHero
